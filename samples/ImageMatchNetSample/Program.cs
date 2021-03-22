8af1271fec196399190ed266fa23c43e5fcd94ee
Yujingfeng <2385419285@qq.com>
2021-03-22 16:11:35 +0800

    修改sample

--- samples/ImageMatchNetSample/Program.cs
+++ samples/ImageMatchNetSample/Program.cs
@@ -1,12 +1,5 @@
 using ImageMatchNet;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
 
 namespace ImageMatchNetSample
 {
@@ -14,50 +7,17 @@ namespace ImageMatchNetSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var path1 = @"E:\projects\ImageMatch.NET\samples\ImageMatchSample\images\1.jpg";
-            var path2 = @"E:\projects\ImageMatch.NET\samples\ImageMatchSample\images\2.jpg";
-
-            var imgs1 = SixLabors.ImageSharp.Image.Load<Rgba32>(path1);
-            var imgs2 = SixLabors.ImageSharp.Image.Load<RgbaVector>(path1);
-            
-            Stopwatch sw = new Stopwatch();
+            var path1 = @"E:\projects\ImageMatchNet\samples\ImageMatchNetSample\images\1.jpg";
+            var path2 = @"E:\projects\ImageMatchNet\samples\ImageMatchNetSample\images\2.jpg";
 
             var gis = new ImageSignature();
 
-            sw.Start();
-            var sign = gis.GenerateSignature(imgs1);
-            sw.Stop();
-            Console.WriteLine(sw.ElapsedMilliseconds);
-            
-            //PrintArray(sign);
+            var sign1 = gis.GenerateSignature(path1);
+            var sign2 = gis.GenerateSignature(path2);
 
-            Console.WriteLine("OK");
+            var dist = gis.NormalizedDistance(sign1, sign2);
+
+            Console.WriteLine(dist);
         }
-
-
-        public static void PrintArray(int[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                int num = arr[i];
-                if ((i + 1) % 24 == 0)
-                {
-                    if (num < 0)
-                        Console.WriteLine($" {num}");
-                    else
-                        Console.WriteLine($"  {num}");
-                }
-                else
-                {
-                    if (num < 0)
-                        Console.Write($" {num}");
-                    else
-                        Console.Write($"  {num}");
-                }
-
-            }
-        }
-
     }
 }
